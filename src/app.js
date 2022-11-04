a7330a281fc65d9a85ff95048345d412d8be6f76
Daniel Jimenez <danieljimenezch@Daniels-MacBook-Pro.local>
2022-11-03 18:32:47 -0600

    add files to repository

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// /* eslint-disable */
+// import "bootstrap";
+// import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+// import "./assets/img/rigo-baby.jpg";
+// import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  
+  document.querySelector("#btn-generate").addEventListener("click", () => {  
+    document.querySelector("#excuse").innerHTML = generateExcuse();
+  });
+
+function generateExcuse () {
+  let who = ['the dog','my granma','his turtle','my bird'];
+  let what = ['eat','pissed','crushed','broked'];
+  let item = ['my homework','my report','my backpack','my car keys','my skateboard']
+  let when = ['before the class','right in time','when I finished','during my lunch','while I was praying'];
+
+  let whoInd = who[Math.floor(Math.random() * who.length )];
+  let whatInd = what[Math.floor(Math.random() * what.length)];
+  let itemInd = item[Math.floor(Math.random() * item.length)]
+  let whenInd = when[Math.floor(Math.random() * when.length)];
+
+  return whoInd + " " + whatInd + " " + itemInd + " " + whenInd;
+}
 };
